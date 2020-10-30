e30824e02e9cf7bba8fb03332d4726e2bd093bc2
fedezaff <73648840+fedezaff@users.noreply.github.com>
2020-10-30 17:38:23 +0100

    Add files via upload

--- frontend-tests-master/script.js
+++ frontend-tests-master/script.js
@@ -7,149 +7,153 @@ $.ajax({
         console.log(ris);
         console.log(ris.pageStructure.layout.lg[9].elements[0].value.urlMp4);
 
+        var base = (ris.pageStructure.layout)
+
         //header
-        document.getElementsByClassName('header-logo')[0].src = (ris.pageStructure.layout.lg[0].elements[0].imagesgroup.images[0].url)
+        $('.header-logo')[0].src = (base.lg[0].elements[0].imagesgroup.images[0].url)
 
-        document.getElementsByClassName('social-menu')[0].innerHTML = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].name)
+        $('.social-menu')[0].innerHTML = (base.lg[0].elements[1].value.menu.items[0].name)
 
-            document.getElementsByClassName('social-menu-link-1')[0].innerHTML = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].subMenu[0].name)
-            document.getElementsByClassName('social-menu-link-1')[0].href = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].subMenu[0].link)
+            $('.social-menu-link-1')[0].innerHTML = (base.lg[0].elements[1].value.menu.items[0].subMenu[0].name)
+            $('.social-menu-link-1')[0].href = (base.lg[0].elements[1].value.menu.items[0].subMenu[0].link)
 
-            document.getElementsByClassName('social-menu-link-2')[0].innerHTML = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].subMenu[1].name)
-            document.getElementsByClassName('social-menu-link-2')[0].href = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].subMenu[1].link)
+            $('.social-menu-link-2')[0].innerHTML = (base.lg[0].elements[1].value.menu.items[0].subMenu[1].name)
+            $('.social-menu-link-2')[0].href = (base.lg[0].elements[1].value.menu.items[0].subMenu[1].link)
 
-            document.getElementsByClassName('social-menu-link-3')[0].innerHTML = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].subMenu[2].name)
-            document.getElementsByClassName('social-menu-link-3')[0].href = (ris.pageStructure.layout.lg[0].elements[1].value.menu.items[0].subMenu[2].link)
+            $('.social-menu-link-3')[0].innerHTML = (base.lg[0].elements[1].value.menu.items[0].subMenu[2].name)
+            $('.social-menu-link-3')[0].href = (base.lg[0].elements[1].value.menu.items[0].subMenu[2].link)
+
+        $('.social-menu-2')[0].innerHTML = (base.lg[0].elements[1].value.menu.items[1].name)
 
         //slide-show (carosello di bootstrap)
-        document.getElementsByClassName('img-slider-homepage')[0].src = (ris.pageStructure.layout.lg[1].elements[0].imagesgroup.images[0].url)
+        $('.img-slider-homepage')[0].src = (base.lg[1].elements[0].imagesgroup.images[0].url)
 
-        document.getElementsByClassName('img-slider-homepage')[1].src = (ris.pageStructure.layout.lg[1].elements[0].imagesgroup.images[1].url)
+        $('.img-slider-homepage')[1].src = (base.lg[1].elements[0].imagesgroup.images[1].url)
 
         //prodotto-evidenza
-        document.getElementsByClassName('img-prodotto-evidenza')[0].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[2].elements[0].imagesgroup.images[0].url)+')'
+        $('.img-prodotto-evidenza')[0].style.backgroundImage = 'url('+(base.lg[2].elements[0].imagesgroup.images[0].url)+')'
 
-        document.getElementsByClassName('prodotto-evidenza')[0].innerHTML += (ris.pageStructure.layout.lg[2].elements[1].value.content)
+        $('.prodotto-evidenza')[0].innerHTML += (base.lg[2].elements[1].value.content)
 
         //nostra-forza
-        document.getElementsByClassName('nostra-forza')[0].innerHTML += (ris.pageStructure.layout.lg[3].elements[0].value.content)
+        $('.nostra-forza')[0].innerHTML += (base.lg[3].elements[0].value.content)
 
         //punti-cardine-text
 
-        document.getElementsByClassName('punti-cardine-text')[0].innerHTML += (ris.pageStructure.layout.lg[4].elements[0].value.content)
+        $('.punti-cardine-text')[0].innerHTML += (base.lg[4].elements[0].value.content)
 
-        document.getElementsByClassName('punti-cardine-text')[0].style.cssText += (ris.pageStructure.layout.lg[4].elements[0].value.settings.inlineStyle)
+        $('.punti-cardine-text')[0].style.cssText += (base.lg[4].elements[0].value.settings.inlineStyle)
 
-        document.getElementsByClassName('punti-cardine-text')[0].style.textAlign+= (ris.pageStructure.layout.lg[4].elements[0].value.settings.textAlignment)
+        $('.punti-cardine-text')[0].style.textAlign+= (base.lg[4].elements[0].value.settings.textAlignment)
 
-        document.getElementsByClassName('punti-cardine-text')[0].style.color += (ris.pageStructure.layout.lg[4].elements[0].value.settings.textColor)
+        $('.punti-cardine-text')[0].style.color += (base.lg[4].elements[0].value.settings.textColor)
 
-        document.getElementsByClassName('punti-cardine-text')[0].style.fontSize += (ris.pageStructure.layout.lg[4].elements[0].value.settings.textSize)
+        $('.punti-cardine-text')[0].style.fontSize += (base.lg[4].elements[0].value.settings.textSize)
 
         //punti-cardine-images
-        document.getElementsByClassName('punti-cardine-images')[0].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[5].elements[0].imagesgroup.images[0].url)+')'
+        $('.punti-cardine-images')[0].style.backgroundImage = 'url('+(base.lg[5].elements[0].imagesgroup.images[0].url)+')'
 
-        document.getElementsByClassName('punti-cardine-images')[1].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[5].elements[0].imagesgroup.images[1].url)+')'
+        $('.punti-cardine-images')[1].style.backgroundImage = 'url('+(base.lg[5].elements[0].imagesgroup.images[1].url)+')'
 
-        document.getElementsByClassName('punti-cardine-images')[2].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[5].elements[0].imagesgroup.images[2].url)+')'
+        $('.punti-cardine-images')[2].style.backgroundImage = 'url('+(base.lg[5].elements[0].imagesgroup.images[2].url)+')'
 
-        document.getElementsByClassName('punti-cardine-images')[3].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[5].elements[0].imagesgroup.images[3].url)+')'
+        $('.punti-cardine-images')[3].style.backgroundImage = 'url('+(base.lg[5].elements[0].imagesgroup.images[3].url)+')'
 
         //prodotti-text
-        document.getElementsByClassName('prodotti-text')[0].innerHTML += (ris.pageStructure.layout.lg[6].elements[0].value.content)
+        $('.prodotti-text')[0].innerHTML += (base.lg[6].elements[0].value.content)
 
-        document.getElementsByClassName('prodotti-text')[0].style.cssText += (ris.pageStructure.layout.lg[6].elements[0].value.settings.inlineStyle)
+        $('.prodotti-text')[0].style.cssText += (base.lg[6].elements[0].value.settings.inlineStyle)
 
-        document.getElementsByClassName('prodotti-text')[0].style.textAlign+= (ris.pageStructure.layout.lg[6].elements[0].value.settings.textAlignment)
+        $('.prodotti-text')[0].style.textAlign+= (base.lg[6].elements[0].value.settings.textAlignment)
 
-        document.getElementsByClassName('prodotti-text')[0].style.color += (ris.pageStructure.layout.lg[6].elements[0].value.settings.textColor)
+        $('.prodotti-text')[0].style.color += (base.lg[6].elements[0].value.settings.textColor)
 
-        document.getElementsByClassName('prodotti-text')[0].style.fontSize += (ris.pageStructure.layout.lg[6].elements[0].value.settings.textSize)
+        $('.prodotti-text')[0].style.fontSize += (base.lg[6].elements[0].value.settings.textSize)
 
         //prodotti-img
 
-        document.getElementsByClassName('prodotti-img')[0].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[7].elements[0].bbswayProducts[0].productImageUrl)+')'
+        $('.prodotti-img')[0].style.backgroundImage = 'url('+(base.lg[7].elements[0].bbswayProducts[0].productImageUrl)+')'
 
-        document.getElementsByClassName('prodotti-img')[1].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[7].elements[0].bbswayProducts[1].url)+')'
+        $('.prodotti-img')[1].style.backgroundImage = 'url('+(base.lg[7].elements[0].bbswayProducts[1].url)+')'
 
-        document.getElementsByClassName('prodotti-img')[2].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[7].elements[0].bbswayProducts[2].url)+')'
+        $('.prodotti-img')[2].style.backgroundImage = 'url('+(base.lg[7].elements[0].bbswayProducts[2].url)+')'
 
-        document.getElementsByClassName('prodotti-img')[3].style.backgroundImage = 'url('+(ris.pageStructure.layout.lg[7].elements[0].bbswayProducts[3].url)+')'
+        $('.prodotti-img')[3].style.backgroundImage = 'url('+(base.lg[7].elements[0].bbswayProducts[3].url)+')'
 
         //mission-text
 
-        document.getElementsByClassName('mission-text')[0].innerHTML += (ris.pageStructure.layout.lg[8].elements[0].value.content)
+        $('.mission-text')[0].innerHTML += (base.lg[8].elements[0].value.content)
 
-        document.getElementsByClassName('mission-text')[0].style.cssText += (ris.pageStructure.layout.lg[8].elements[0].value.settings.inlineStyle)
+        $('.mission-text')[0].style.cssText += (base.lg[8].elements[0].value.settings.inlineStyle)
 
-        document.getElementsByClassName('mission-text')[0].style.textAlign+= (ris.pageStructure.layout.lg[8].elements[0].value.settings.textAlignment)
+        $('.mission-text')[0].style.textAlign+= (base.lg[8].elements[0].value.settings.textAlignment)
 
-        document.getElementsByClassName('mission-text')[0].style.color += (ris.pageStructure.layout.lg[8].elements[0].value.settings.textColor)
+        $('.mission-text')[0].style.color += (base.lg[8].elements[0].value.settings.textColor)
 
-        document.getElementsByClassName('mission-text')[0].style.fontSize += (ris.pageStructure.layout.lg[8].elements[0].value.settings.textSize)
+        $('.mission-text')[0].style.fontSize += (base.lg[8].elements[0].value.settings.textSize)
 
         //prodotti
 
-        document.getElementsByClassName('prodotti-video')[0].src = (ris.pageStructure.layout.lg[9].elements[0].value.urlMp4)
+        $('.prodotti-video')[0].src = (base.lg[9].elements[0].value.urlMp4)
 
-        document.getElementsByClassName('prodotti-video-text')[0].innerHTML += (ris.pageStructure.layout.lg[9].elements[1].value.content)
+        $('.prodotti-video-text')[0].innerHTML += (base.lg[9].elements[1].value.content)
 
         //blog-subtitle
 
-        document.getElementsByClassName('blog-subtitle')[0].innerHTML += (ris.pageStructure.layout.lg[10].elements[0].value.content)
+        $('.blog-subtitle')[0].innerHTML += (base.lg[10].elements[0].value.content)
 
-        document.getElementsByClassName('blog-subtitle')[0].style.cssText += (ris.pageStructure.layout.lg[10].elements[0].value.settings.inlineStyle)
+        $('.blog-subtitle')[0].style.cssText += (base.lg[10].elements[0].value.settings.inlineStyle)
 
-        document.getElementsByClassName('blog-subtitle')[0].style.textAlign+= (ris.pageStructure.layout.lg[10].elements[0].value.settings.textAlignment)
+        $('.blog-subtitle')[0].style.textAlign+= (base.lg[10].elements[0].value.settings.textAlignment)
 
-        document.getElementsByClassName('blog-subtitle')[0].style.color += (ris.pageStructure.layout.lg[10].elements[0].value.settings.textColor)
+        $('.blog-subtitle')[0].style.color += (base.lg[10].elements[0].value.settings.textColor)
 
-        document.getElementsByClassName('blog-subtitle')[0].style.fontSize += (ris.pageStructure.layout.lg[10].elements[0].value.settings.textSize)
+        $('.blog-subtitle')[0].style.fontSize += (base.lg[10].elements[0].value.settings.textSize)
 
         //blog-title
 
-        document.getElementsByClassName('blog-title')[0].innerHTML += (ris.pageStructure.layout.lg[11].elements[0].value.content)
+        $('.blog-title')[0].innerHTML += (base.lg[11].elements[0].value.content)
 
-        document.getElementsByClassName('blog-title')[0].style.cssText += (ris.pageStructure.layout.lg[11].elements[0].value.settings.inlineStyle)
+        $('.blog-title')[0].style.cssText += (base.lg[11].elements[0].value.settings.inlineStyle)
 
-        document.getElementsByClassName('blog-title')[0].style.textAlign+= (ris.pageStructure.layout.lg[11].elements[0].value.settings.textAlignment)
+        $('.blog-title')[0].style.textAlign+= (base.lg[11].elements[0].value.settings.textAlignment)
 
-        document.getElementsByClassName('blog-title')[0].style.color += (ris.pageStructure.layout.lg[11].elements[0].value.settings.textColor)
+        $('.blog-title')[0].style.color += (base.lg[11].elements[0].value.settings.textColor)
 
-        document.getElementsByClassName('blog-title')[0].style.fontSize += (ris.pageStructure.layout.lg[11].elements[0].value.settings.textSize)
+        $('.blog-title')[0].style.fontSize += (base.lg[11].elements[0].value.settings.textSize)
 
         //blog
 
-        document.getElementsByClassName('blog-1-intro')[0].innerHTML += (ris.pageStructure.layout.lg[12].elements[0].posts[0].intro)
+        $('.blog-1-intro')[0].innerHTML += (base.lg[12].elements[0].posts[0].intro)
 
-        document.getElementsByClassName('blog-2-intro')[0].innerHTML += (ris.pageStructure.layout.lg[12].elements[0].posts[1].intro)
+        $('.blog-2-intro')[0].innerHTML += (base.lg[12].elements[0].posts[1].intro)
 
-        document.getElementsByClassName('blog-3-intro')[0].innerHTML += (ris.pageStructure.layout.lg[12].elements[0].posts[2].intro)
-
-
-        document.getElementsByClassName('blog-1-title')[0].innerHTML += (ris.pageStructure.layout.lg[12].elements[0].posts[0].title)
-
-        document.getElementsByClassName('blog-2-title')[0].innerHTML += (ris.pageStructure.layout.lg[12].elements[0].posts[1].title)
-
-        document.getElementsByClassName('blog-3-title')[0].innerHTML += (ris.pageStructure.layout.lg[12].elements[0].posts[2].title)
+        $('.blog-3-intro')[0].innerHTML += (base.lg[12].elements[0].posts[2].intro)
 
 
-        document.getElementsByClassName('blog-1-img')[0].style.backgroundImage += 'url('+(ris.pageStructure.layout.lg[12].elements[0].posts[0].urlImage)+')'
+        $('.blog-1-title')[0].innerHTML += (base.lg[12].elements[0].posts[0].title)
 
-        document.getElementsByClassName('blog-2-img')[0].style.backgroundImage += 'url('+(ris.pageStructure.layout.lg[12].elements[0].posts[1].urlImage)+')'
+        $('.blog-2-title')[0].innerHTML += (base.lg[12].elements[0].posts[1].title)
 
-        document.getElementsByClassName('blog-3-img')[0].style.backgroundImage += 'url('+(ris.pageStructure.layout.lg[12].elements[0].posts[2].urlImage)+')'
+        $('.blog-3-title')[0].innerHTML += (base.lg[12].elements[0].posts[2].title)
+
+
+        $('.blog-1-img')[0].style.backgroundImage += 'url('+(base.lg[12].elements[0].posts[0].urlImage)+')'
+
+        $('.blog-2-img')[0].style.backgroundImage += 'url('+(base.lg[12].elements[0].posts[1].urlImage)+')'
+
+        $('.blog-3-img')[0].style.backgroundImage += 'url('+(base.lg[12].elements[0].posts[2].urlImage)+')'
 
         //footer
 
-        document.getElementsByClassName('footer')[0].innerHTML += (ris.pageStructure.layout.lg[13].elements[0].value.content)
+        $('.footer')[0].innerHTML += (base.lg[13].elements[0].value.content)
 
-        document.getElementsByClassName('footer')[0].style.cssText += (ris.pageStructure.layout.lg[13].elements[0].value.settings.inlineStyle)
+        $('.footer')[0].style.cssText += (base.lg[13].elements[0].value.settings.inlineStyle)
 
-        document.getElementsByClassName('footer')[0].style.textAlign+= (ris.pageStructure.layout.lg[13].elements[0].value.settings.textAlignment)
+        $('.footer')[0].style.textAlign+= (base.lg[13].elements[0].value.settings.textAlignment)
 
-        document.getElementsByClassName('footer')[0].style.color += (ris.pageStructure.layout.lg[13].elements[0].value.settings.textColor)
+        $('.footer')[0].style.color += (base.lg[13].elements[0].value.settings.textColor)
 
-        document.getElementsByClassName('footer')[0].style.fontSize += (ris.pageStructure.layout.lg[13].elements[0].value.settings.textSize)
+        $('.footer')[0].style.fontSize += (base.lg[13].elements[0].value.settings.textSize)
     }
 })
